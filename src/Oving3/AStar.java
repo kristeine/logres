7e077d47e9de606cdcadda6150275d5840d16e9a
Kristine <krstein@stud.ntnu.no>
2013-10-04 11:10:52 +0200

    Initial commit

---
+++ src/Oving3/AStar.java
@@ -0,0 +1,11 @@
+package Oving3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kristine
+ * Date: 10/4/13
+ * Time: 11:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class AStar {
+}
