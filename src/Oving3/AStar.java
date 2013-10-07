c4bcb864967f2ea856e3383bb689deb1baab6062
Kristine <krstein@stud.ntnu.no>
2013-10-07 17:58:40 +0200

    Improvements

--- src/Oving3/AStar.java
+++ src/Oving3/AStar.java
@@ -14,6 +14,8 @@ public class AStar{
 	private ArrayList<SearchNode> closed;
 	private ArrayList<SearchNode> open;
 	private HashMap<SearchNode, SearchNode> path;
+	private ArrayList<String> goalBoard = new ArrayList<String>();
+	private int generatedNodes;
 
 	public  static void main(String[] args) {
 		AStar aStar = new AStar();
@@ -24,6 +26,9 @@ public class AStar{
 	}
 
 	public String find(SearchNode start, SearchNode goal){
+		goalBoard.add("red"); goalBoard.add("red"); goalBoard.add("red"); goalBoard.add("");
+		goalBoard.add("grey"); goalBoard.add("grey"); goalBoard.add("grey");
+		generatedNodes = 0;
 		closed = new ArrayList<SearchNode>();
 		open = new ArrayList<SearchNode>(); open.add(start);
 		path = new HashMap<SearchNode, SearchNode>();
@@ -31,12 +36,20 @@ public class AStar{
 		start.f = start.g + heuristicCostEstimate(start, goal);
 
 		while (!open.isEmpty()) {
-			SearchNode current = open.get(0); //assume open sorted on f
-			if (current == goal) {
+			int tentative_low = 6; SearchNode current = open.get(0);
+			for (SearchNode sn : open) {   //find the node with lowest f value
+				if (sn.f < tentative_low) {
+					tentative_low = sn.f; current = sn;
+				}
+			}
+
+			//SearchNode current = open.get(0); //assume open sorted on f
+			if (current.getBoard() == goalBoard) {   //check if node is goal
 				System.out.println("Found the goal");
 				return path(path, goal);
 			}
 			open.remove(current); closed.add(current);
+			generatedNodes += current.generateChildren();
 
 			for (SearchNode child : current.getChildren()) {
 				System.out.println("Checking child of: " + current.color);
@@ -70,5 +83,9 @@ public class AStar{
 		else {
 			return goal.toString();
 		}
+	}
+
+	public int getGeneratedNodes() {
+		return generatedNodes;
 	}
 }
--- src/Oving3/SearchNode.java
+++ src/Oving3/SearchNode.java
@@ -14,10 +14,9 @@ public class SearchNode {
 	public int g; //cost of arrival
 	public int h; //estimated cost to goal
 	public int f; //g+h, estimated total cost to goal
-	private boolean open;
-	private SearchNode parent;
 	private ArrayList<SearchNode> children = new ArrayList<SearchNode>();
 	public String color;
+	private ArrayList<String> board = new ArrayList<String>();
 	private int position;
 
 	public SearchNode() {}
@@ -26,17 +25,8 @@ public class SearchNode {
 		this.color = color;
 	}
 
-	public SearchNode(int pos) {
-		position = pos;
-	}
-
-	public String getStatus() {
-		if (open) {return "open";}
-		else {return "closed";}
-	}
-
-	public SearchNode getParent() {
-		return parent;
+	public SearchNode(ArrayList<String> board) {
+		this.board = board;
 	}
 
 	public ArrayList<SearchNode> getChildren(){
@@ -51,8 +41,44 @@ public class SearchNode {
 		return color;
 	}
 
-	public void generateChildren(){
+	public int generateChildren(){
+		int possibleMoves = 0;
+		int blankPosition = 0;
+		for (int i = 0; i < board.size(); i++) {
+			if (board.get(i) == "") { //find empty space
+				if (i == 0 || i == board.size()-1) {   //edge position
+					possibleMoves = 2;
+				}
+				else if (i == 1 || i == board.size()-2) { //close to edge
+					possibleMoves =  3;
+				}
+				else {
+					possibleMoves =  4;
+				}
+				blankPosition = i;
+				break;
+			}
+		}
+		if (blankPosition == 0) {
+			children.add(newChild(board, 0, 1));
+			children.add(newChild(board, 0, 2));
+
+		}
 		//sjekke brettet
-		//for hvert mulige trekk for denne noden: opprett barnenode
+		//for hvert mulige trekk for denne noden: opprett barnenode med nytt board
+		//returner antall opprettede noder
+		return possibleMoves;
+	}
+
+	private SearchNode newChild(ArrayList<String> board, int blankPosition, int valuePosition) {
+		ArrayList<String> newBoard = board;
+		newBoard.set(blankPosition, newBoard.get(valuePosition));
+		newBoard.set(valuePosition, "");
+		SearchNode child = new SearchNode(newBoard);
+		return child;
+	}
+
+	public ArrayList<String> getBoard() {
+		return board;
 	}
 }
