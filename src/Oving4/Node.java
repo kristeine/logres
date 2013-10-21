e23b5fbc4c35cf03aa0570bcc0c68f3c591ac52f
Kristine <kristine.webkom@gmail.com>
2013-10-21 11:11:32 +0200

    Initial commit

---
+++ src/Oving4/Node.java
@@ -0,0 +1,13 @@
+package Oving4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kristine
+ * Date: 10/21/13
+ * Time: 11:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class Node {
+	// Board
+	// Neighbour generation
+}
---
+++ src/Oving4/SimulatedAnnealing.java
@@ -0,0 +1,15 @@
+package Oving4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kristine
+ * Date: 10/21/13
+ * Time: 11:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimulatedAnnealing {
+	// Objective function
+	// Acceptance probability function
+	// General algorithm
+	//
+}
