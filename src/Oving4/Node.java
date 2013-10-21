86db7e5150bc94c2d7e6bf7c27291999013f14a5
Kristine <kristine.webkom@gmail.com>
2013-10-21 11:59:37 +0200

    Added plans for eighbour generation

--- src/Oving4/Node.java
+++ src/Oving4/Node.java
@@ -12,9 +12,11 @@ import java.util.Random;
  */
 public class Node {
 	int[][] board;
+	int k;
 	// Neighbour generation
 
 	public Node(boolean initial, int width, int k) {
+		this.k = k;
 		if (initial) {
 			// Empty board
 			board = new int[width][width];
@@ -34,18 +36,17 @@ public class Node {
 		}
 	}
 
-	public void generateNeighbours(){
-		move();
-	}
-
 	public void move() {
-		Random rng = new Random();
-
-		// For each row:
-		int indexToMove = rng.nextInt(1);
-		// Move the chosen index to another column on the same row
+		// For each threatened egg:
+		// For width-k:    (number of possible moves within the same row)
+		// Make a new node where that move has been made
 	}
 
+	public boolean isThreatened(int i, int j){
+		// Check if that position is threatened
+		// I.e: whether there are x > k eggs on this position's row or column or diagonal
+		return false;
+	}
 	public boolean equals(Node other) {
 		return this.board.equals(other.board);
 	}
